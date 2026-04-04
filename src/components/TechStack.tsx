b6f60bd703757e5ecef22d5e235d5c3a3d8eeee6
PANKAJ GUSAIN <pankajgusain990@gmail.com>
2026-04-05 02:33:27 +0530

    Update TechStack.tsx

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -34,7 +34,7 @@ const textures = imageUrls.map((url) => textureLoader.load(url));
 
 const sphereGeometry = new THREE.SphereGeometry(1, 28, 28);
 
-const spheres = [...Array(30)].map(() => ({
+const spheres = [...Array(16)].map(() => ({
   scale: [0.7, 1, 0.8, 1, 1][Math.floor(Math.random() * 5)],
 }));
 
