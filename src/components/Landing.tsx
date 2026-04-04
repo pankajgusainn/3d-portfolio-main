5f2bddfc5ab25bbb7e71790627c0c3eb0368fae2
PANKAJ GUSAIN <pankajgusain990@gmail.com>
2026-04-05 00:42:32 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,9 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              PANKAJ
+              Pankaj
               <br />
-              <span>GUSAIN</span>
+              <span>Gusain</span>
             </h1>
           </div>
           <div className="landing-info">
