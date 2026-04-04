0b4fa04ab35e69f6388abbb33bd2f17dbe5fef3e
PANKAJ GUSAIN <pankajgusain990@gmail.com>
2026-04-05 02:00:17 +0530

    image updates

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -13,14 +13,22 @@ import {
 
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
-  "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
-  "/images/javascript.webp",
+  "/images/aws.webp",
+  "/images/cicd.webp",
+  "/images/cloud.webp",
+  "/images/devops.webp",
+  "/images/docker.webp",
+  "/images/git.webp",
+  "/images/gitbash.webp",
+  "/images/github_action.webp",
+  "/images/github.webp",
+  "/images/gitlab.webp",
+  "/images/grafana.webp",
+  "/images/jenkins.webp",
+  "/images/kubernetes.webp",
+  "/images/linux.webp",
+  "/images/terraform.webp",
+  "/images/cli.webp",
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
